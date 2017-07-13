4a8a2f1df7e7a51a16bd069441afe166ab01896f
Jeff Schnitzer <jeff@infohazard.org>
2017-07-13 13:46:01 -0700

    JSONUserType had the wrong generic type

--- postguice/src/main/java/com/voodoodyne/postguice/usertype/TreeSetStringUserType.java
+++ postguice/src/main/java/com/voodoodyne/postguice/usertype/TreeSetStringUserType.java
@@ -2,13 +2,12 @@ package com.voodoodyne.postguice.usertype;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 
-import java.util.List;
 import java.util.TreeSet;
 
 /**
  * UserType which lets us store JsonNode in hibernate, converting to JSON natively.
  */
-public class TreeSetStringUserType extends JSONUserType<List<String>> {
+public class TreeSetStringUserType extends JSONUserType<TreeSet<String>> {
 	public TreeSetStringUserType() {
 		super(new TypeReference<TreeSet<String>>(){});
 	}
